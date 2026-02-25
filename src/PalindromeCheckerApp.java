686b6617cad488fd7939bfbc46f0d93328216daa
hb8049-Harsh <hb8049@srmist.edu.in>
2026-02-25 10:23:42 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,30 @@
 public class PalindromeCheckerApp {
+
+    private static final String APP_NAME = "Palindrome Checker App";
+    private static final String APP_VERSION = "Version 1.0";
+
+    public static void main(String[] args) {
+        uc1();
+//        uc2();
+//        uc3();
+//        uc4();
+//        uc5();
+//        uc6();
+//        uc7();
+//        uc8();
+//        uc9();
+//        uc10();
+//        uc11();
+//        uc12();
+//        uc13();
+        System.out.println("Program Finished.");
+    }
+
+    public static void uc1() {
+        System.out.println("=================================");
+        System.out.println("Welcome to the Application");
+        System.out.println("Application Name: " + APP_NAME);
+        System.out.println("Application Version: " + APP_VERSION);
+        System.out.println("=================================");
+    }
 }
