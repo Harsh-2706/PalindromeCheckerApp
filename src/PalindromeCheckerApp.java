47764484591b44bdfb89211b6e74940f0b582ea5
hb8049-Harsh <hb8049@srmist.edu.in>
2026-02-25 11:18:58 +0530

    UC5:Stack Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,20 +4,24 @@ public class PalindromeCheckerApp {
     private static final String APP_VERSION = "Version 1.0";
 
     public static void main(String[] args) {
-        uc3();
+        uc5();
         System.out.println("Program Finished.");
     }
 
-    public static void uc3() {
-        String original = "level";
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+    public static void uc5() {
+        String word = "deed";
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome.");
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
         } else {
-            System.out.println(original + " is NOT a Palindrome.");
+            System.out.println(word + " is NOT a Palindrome.");
         }
     }
 }
