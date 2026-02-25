65c9b35f4912396924bef4066f60fbe32d53f6c0
hb8049-Harsh <hb8049@srmist.edu.in>
2026-02-25 11:28:06 +0530

    UC8: Linked List

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,22 +4,53 @@ public class PalindromeCheckerApp {
     private static final String APP_VERSION = "Version 1.0";
 
     public static void main(String[] args) {
-        uc7();
+        uc8();
         System.out.println("Program Finished.");
     }
 
-    public static void uc7() {
-        String word = "racecar";
-        java.util.Deque<Character> deque = new java.util.LinkedList<>();
-        for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
+    public static void uc8() {
+        String word = "madam";
+        class Node {
+            char data;
+            Node next;
+            Node(char data) { this.data = data; }
         }
+        Node head = null;
+        Node tail = null;
+        for (char ch : word.toCharArray()) {
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node prev = null;
+        Node current = slow;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        Node secondHalf = prev;
+        Node firstHalf = head;
         boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 isPalindrome = false;
                 break;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
         if (isPalindrome) {
             System.out.println(word + " is a Palindrome.");
