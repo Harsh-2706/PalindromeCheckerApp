9b1479a3c2a934e2b9daf21abb270137ce26fafb
hb8049-Harsh <hb8049@srmist.edu.in>
2026-02-25 11:01:13 +0530

    UC4: Character Array Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,20 +4,23 @@ public class PalindromeCheckerApp {
     private static final String APP_VERSION = "Version 1.0";
 
     public static void main(String[] args) {
-        uc3();
+        uc4();
         System.out.println("Program Finished.");
     }
 
-    public static void uc3() {
-        String original = "level";
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
-        }
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome.");
-        } else {
-            System.out.println(original + " is NOT a Palindrome.");
+    public static void uc4() {
+        String word = "radar";
+        char[] characters = word.toCharArray();
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
     }
 }
