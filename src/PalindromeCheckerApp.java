749f2202731c3138a018b82edaec0ecfd838657f
Harsh-2706 <harsh.bubna06@gmail.com>
2026-03-12 09:59:06 +0530

    UC11: Object-oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,53 +4,54 @@ public class PalindromeCheckerApp {
     private static final String APP_VERSION = "Version 1.0";
 
     public static void main(String[] args) {
-        uc10();
+        uc11();
         System.out.println("Program Finished.");
     }
 
     import java.util.Scanner;
 
-    public static void uc10) {
+    public static void uc11() {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Case-Insensitive & Space-Ignored Palindrome Checker ===");
+        System.out.println("=== Object-Oriented Palindrome Checker ===");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Normalize the string
-        String normalized = normalizeString(input);
+        // Create object of PalindromeChecker class
+        PalindromeChecker checker = new PalindromeChecker();
 
-        boolean result = isPalindromeRecursive(normalized, 0, normalized.length() - 1);
+        boolean result = checker.checkPalindrome(input);
 
         if(result) {
-            System.out.println("The string is a Palindrome (ignoring spaces and case).");
-        }
-        else {
+            System.out.println("The string is a Palindrome.");
+        } else {
             System.out.println("The string is NOT a Palindrome.");
         }
     }
+}
 
-    // String preprocessing using regex
-    public static String normalizeString(String str) {
-        str = str.toLowerCase();              // ignore case
-        str = str.replaceAll("[^a-z0-9]", ""); // remove spaces & special characters
-        return str;
-    }
+// Service class (Encapsulation)
+class PalindromeChecker {
 
-    public static boolean isPalindromeRecursive(String str, int start, int end) {
+    public boolean checkPalindrome(String input) {
 
-        // Base condition
-        if(start >= end) {
-            return true;
+        // Normalize string
+        String normalized = input.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        char[] arr = normalized.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+            if(arr[start] != arr[end]) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
-        // Check mismatch
-        if(str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindromeRecursive(str, start + 1, end - 1);
+        return true;
     }
 }
